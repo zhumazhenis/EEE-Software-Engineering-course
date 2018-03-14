be683733d6c60c65eabce8f76d4bbd28e47a4185
Zhumazhenis Dairabay <zhumazhenis@gmail.com>
2018-03-15 04:05:38 +0600

    Create DiceGameTest.java

---
+++ Lab6/DiceGameTest.java
@@ -0,0 +1,13 @@
+
+/*
+ * Done by Zhumazhenis Dairabay
+ * Lab 6
+ * Exercise 1
+ */
+
+public class DiceGameTest {
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.play();
+	}
+}
